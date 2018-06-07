46d78a0464b9f7830d126fc7be39f411c85f2053
Will Wang <willwang@Wills-MacBook-Pro.local>
2018-06-07 11:02:49 -0700

    added command

--- tests/detect.py
+++ tests/detect.py
@@ -1,5 +1,7 @@
 import subprocess
 
+command = "kubectl get service hotrod-frontend -o jsonpath='{.status.loadBalancer.ingress[0].ip}' -n "
+
 def detect(namespace):
     while True:
         value = subprocess.check_output(command + namespace, shell=True).strip()
