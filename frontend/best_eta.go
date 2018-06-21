2e8a794ee9c30520b883014627652f235ffe8fb7
Kevin Lin <kevin@kelda.io>
2018-06-21 13:08:47 -0700

    eta: Fix ETA response logging
    Before it always logged the response for the closest driver.

--- frontend/best_eta.go
+++ frontend/best_eta.go
@@ -73,7 +73,7 @@ func (eta *bestETA) Get(ctx context.Context, customerID string) (*Response, erro
 		if result.err != nil {
 			return nil, err
 		}
-		log.WithField("driver", resp.Driver).WithField("eta", resp.ETA.String()).Info("Driver time")
+		log.WithField("driver", result.Driver).WithField("eta", result.ETA.String()).Info("Driver time")
 		if result.route.ETA < bestETAResp.ETA {
 			bestETAResp.ETA = result.route.ETA
 			bestETAResp.Driver = result.driver
